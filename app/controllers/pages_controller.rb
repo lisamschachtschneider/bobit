e3381814bd56fccb0e0874fa73fdf42c20c973c8
Lisa Schachtschneider <islisamarie@gmail.com>
2014-10-20 17:10:04 +0200

    adding style to view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,16 @@ class PagesController < ApplicationController
   def index
   end
 
+  def bands
+  end
+
+  def songs
+  end
+
   def about
   end
   
+  def legal
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'about' => 'pages#about'
+  get 'bands' => 'pages#bands'
+  get 'songs' => 'pages#songs'
+  get 'legal' => 'pages#legal'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
