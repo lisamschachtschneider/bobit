7c5695f34b98fdc3a306cff55bbba2237a92a5d9
Lisa Schachtschneider <islisamarie@gmail.com>
2014-10-21 11:50:38 +0200

    adding songs controller with delete, index, upload views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/upload'
+
+  get 'songs/delete'
+
   root 'pages#index'
   get 'about' => 'pages#about'
   get 'bands' => 'pages#bands'
