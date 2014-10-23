a799e435873340f675c581f0b669f3816f2450f0
Lisa Schachtschneider <islisamarie@gmail.com>
2014-10-23 11:53:08 +0200

    setting locale for top level domain name and subdomain

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,46 @@
+class SongsController < ApplicationController
+#attr_accessible :song_id, :name
+
+  def index
+    @songs = Song.all
+    flash[:notice] = "Hello Flash"
+  end
+
+  def list
+    @songs = Song.find(:all)
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+
+  def upload
+  end
+
+  def delete
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    # @song = Song.new(params[:song])
+    @song = Song.new(song_params)
+
+    if @song.save
+      flash[:notice] = "Your song was successfully uploaded!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Your song was not uploaded!"
+      redirect_to "songs#pages"
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :title)
+  end
+end
