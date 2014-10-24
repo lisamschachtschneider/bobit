ebd475fccb83e0f646eb28d31d87c990364243ac
Lisa Schachtschneider <islisamarie@gmail.com>
2014-10-24 10:25:43 +0200

    removing unnec. methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,4 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[ :locale ] || I18n.default_locale
   end
-
-  def extract_locale_from_tld
-    parsed_locale = request.host.split('.').last
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale
-    : nil
-  end
-
-  def extract_locale_from_subdomain
-    parsed_locale = request.subdomains.first
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale
-    : nil
-  end
-  
-
 end
