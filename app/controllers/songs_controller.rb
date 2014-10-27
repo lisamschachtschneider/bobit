e1e26a07a89644c7a169c08635874f39021b0b97
Lisa Schachtschneider <islisamarie@gmail.com>
2014-10-27 16:05:16 +0100

    adding destroy feature

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,6 +32,14 @@ class SongsController < ApplicationController
   def delete
   end
 
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+    redirect_to songs_path
+  end
+  
+
   def new
     @song = Song.new
   end
