eb9bdec7a92dbfc7186f3398d83fc613c527bcf1
Lisa Schachtschneider <islisamarie@gmail.com>
2014-10-28 11:47:49 +0100

    removed unnecesaary mininum

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
   mount_uploader :file, SongUploader
   validates :file, presence: true
-  validates :title, :artist, presence: true, length: { minimum: 1 }
+  validates :title, :artist, presence: true
 end
