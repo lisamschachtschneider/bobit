a275f18864c213f38d7df3bc0fba329e9e0eb619
Lisa Schachtschneider <islisamarie@gmail.com>
2014-11-03 17:00:28 +0100

    join tables bands and songs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103145829) do
+ActiveRecord::Schema.define(version: 20141103155924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20141103145829) do
     t.text     "news"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "bands_songs", id: false, force: true do |t|
+    t.integer "band_id", null: false
+    t.integer "song_id", null: false
   end
 
   create_table "songs", force: true do |t|
