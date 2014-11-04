3c45f1054c4311e7abec3baee8838a99df92c619
Lisa Schachtschneider <islisamarie@gmail.com>
2014-11-04 09:33:45 +0100

    adding has and belongs to many relationship between song and bands

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
+  has_and_belongs_to_many :bands
   mount_uploader :file, SongUploader
   validates :file, presence: true
   validates :title, :artist, presence: true
