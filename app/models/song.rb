5615e54763e7941e63c53cecdca723055d104273
Lisa Schachtschneider <islisamarie@gmail.com>
2014-11-11 11:12:43 +0100

    adding associations to song and playlist model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   has_and_belongs_to_many :bands
+  belongs_to :playlists
   accepts_nested_attributes_for :bands, allow_destroy: true
   mount_uploader :file, SongUploader
   validates :file, :title, presence: true
