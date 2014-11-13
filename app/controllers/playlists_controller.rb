9442e2adaa7926d3efd439c89c584eaa58d22b8a
Lisa Schachtschneider <islisamarie@gmail.com>
2014-11-13 16:06:41 +0100

    adding form to create new playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,30 @@
 class PlaylistsController < ApplicationController
+  def new
+    @playlist = Playlist.new
+    @songs = Song.all
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    if @playlist.save
+      redirect_to playlists_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  def index
+    @playlists = Playlist.all
+  end
+
+  def show
+  end
+
+private
+  def playlist_params
+    params.require(:playlist).permit(:title, :song_attributes => [:title, :file])
+  end
 end
