422999cf851bd7b45b529fbae696dcde61059363
Lisa Schachtschneider <islisamarie@gmail.com>
2014-11-13 16:30:26 +0100

    adding playlist_id to songs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111100700) do
+ActiveRecord::Schema.define(version: 20141113152555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141111100700) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "file"
+    t.integer  "playlist_id"
   end
 
 end
