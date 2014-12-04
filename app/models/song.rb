d4711d994e8ee9b3d2514d68f81f625a33bee866
Lisa Schachtschneider <islisamarie@gmail.com>
2014-12-04 09:08:45 +0100

    changing song associations to polymorphic

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,5 @@
 class Song < ActiveRecord::Base
-  has_and_belongs_to_many :bands
-  belongs_to :playlist
-  accepts_nested_attributes_for :bands, allow_destroy: true
+  belongs_to :audioable, polymorphic: true
   mount_uploader :file, SongUploader
   validates :file, :title, presence: true
 
