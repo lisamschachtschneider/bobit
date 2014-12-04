4d4bf0fed50cdd346497c739d70a28dd0c1e4497
Lisa Schachtschneider <islisamarie@gmail.com>
2014-12-04 16:50:19 +0100

    adding validations and nested attributes

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,5 @@
 class Band < ActiveRecord::Base
   has_many :songs, as: :audioable
   accepts_nested_attributes_for :songs, allow_destroy: true
-  validates :name, :description, presence: true
+#  validates :name, :description, presence: true
 end
