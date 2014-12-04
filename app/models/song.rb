e20c00bad31ff80ff461e2793ec8096915f346e1
Lisa Schachtschneider <islisamarie@gmail.com>
2014-12-04 10:04:17 +0100

    HABTM playlists

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :audioable, polymorphic: true
+  has_and_belongs_to_many :playlists
   mount_uploader :file, SongUploader
   validates :file, :title, presence: true
 
