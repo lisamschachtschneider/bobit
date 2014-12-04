819803c0583ad353fd0947e15e2b5bb647a8e867
Lisa Schachtschneider <islisamarie@gmail.com>
2014-12-04 10:02:40 +0100

    creating artist model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111100700) do
+ActiveRecord::Schema.define(version: 20141204081530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "bands", force: true do |t|
     t.string   "name"
