99812e88608f33d8bf050ceda5c94f9b39a1a03d
Lisa Schachtschneider <islisamarie@gmail.com>
2014-12-05 14:47:11 +0100

    addings songs as a resource of bands, playlists, and artists

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about' => 'pages#about'
   get 'legal' => 'pages#legal'
-  resources :songs, :bands, :playlists, :artists
+  resources :bands, :playlists, :artists do
+    resources :songs 
+  end
 end
