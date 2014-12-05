9b00f5d025d60a6a541c2b2d67b00aa23d0c1fd8
Lisa Schachtschneider <islisamarie@gmail.com>
2014-12-05 14:17:39 +0100

    adding artists routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about' => 'pages#about'
   get 'legal' => 'pages#legal'
-  resources :songs, :bands, :playlists
+  resources :songs, :bands, :playlists, :artists
 end
