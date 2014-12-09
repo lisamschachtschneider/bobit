8c525988355e988271cac4e64676d6812e87f9dd
Lisa Schachtschneider <islisamarie@gmail.com>
2014-12-09 13:50:05 +0100

    adding index, show, new and edit to artists

--- db/migrate/20141204091315_create_playlists_and_songs.rb
+++
@@ -1,8 +0,0 @@
-class CreatePlaylistsAndSongs < ActiveRecord::Migration
-  def change
-    create_table :playlists_and_songs do |t|
-      t.belongs_to :song, index: true
-      t.belongs_to :playlist, index: true
-    end
-  end
-end
--- db/migrate/20141204091315_create_playlists_songs.rb
+++
@@ -1,8 +0,0 @@
-class CreatePlaylistsSongs < ActiveRecord::Migration
-  def change
-    create_table :playlists_songs, id: false do |t|
-      t.belongs_to :song
-      t.belongs_to :playlist
-    end
-  end
-end
